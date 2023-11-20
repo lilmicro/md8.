8b1565b80d3b72ab9a2f466cf3ec9a2d935e328f
lilmicro <epaskevica3@gmail.com>
2023-11-20 22:05:10 +0000

    putra1

--- md8/src/scripts.ts
+++ md8/src/scripts.ts
@@ -1,30 +1,106 @@
-import $ from 'jquery';
+import $, { data } from 'jquery';
 import sum from './utils/sum/sum';
 
 
-import axios, { AxiosResponse } from 'axios';
 
-const addButton = document.querySelector(".js-add-*^*");
-const wrappers = document.querySelector(".js-ph-wrapper");
-type Moods = {
-    id: number;
-    name: string;
-}
+
+
+//const addButton = document.querySelector(".js-add-button");
+
+
 //const url = 'http://localhost:3004/moods';
-const url = 'http://localhost:3004/moods/1'; //dabus konkretu
+//const url = 'http://localhost:3004/moods/1'; //dabus konkretu
 
-const result: Promise<AxiosResponse<any>> = axios.get<Moods[]>(url 'http://localhost:3004/moods');
+//const result: Promise<AxiosResponse<any>> = axios.get<Moods[]>(url: 'http://localhost:3004/moods');
+
+//result.then((res) =>{
+//  console.log(res.data)
+//})
 
 
-result.then(({data}) :void =>{
-    data.forEach((moods : Moods) => {
-        wrappers.innerHTML = 
-         <h1>moods</h1>;
+import axios, { AxiosError, AxiosResponse } from 'axios';
+
+type mood = {
+    id: number;
+    url: string;
+    name: string;
+    name2: string;
+    createdDate: string;
+}
+
+
+const dropmoods = ()=>{
+const result = axios.get<mood[]>(' http://localhost:3004/moods ');
+const imageInput = document.querySelector<HTMLInputElement>('.js-upload-input');
+        
+const wrappers = document.querySelector<HTMLDivElement>(".js-ph-wrapper");
+ wrappers.innerHTML = '';
+
+    result.then(({data}) :void =>{
+    data.forEach((moods : mood) => {
+        wrappers.innerHTML += `
+        <div class="mood">
+        <div class="image">
+        <img src="${moods.url}" width="150" height="150" >
+        </div>
+         <h1 class= "text1">${moods.name}</h1>
+         <br>
+         <h1 class= "text2">${moods.name2}</h1>
+         <button class="js-moods-delete" data-moods-id="${moods.id}">delete</button>
+         <hr>
+         </div>
+         ` 
     });
-})
 
-addButton.addEventListener("click", () =>{
-    axios.post( 'http://localhost:3004/moods', {
+
+
+const moodDelete = document.querySelectorAll<HTMLButtonElement>('.js-moods-delete');
+    moodDelete.forEach((moodBtn: HTMLButtonElement) =>{
+        moodBtn.addEventListener('click', async () =>{
+           const moodsId   = moodBtn.dataset;
+           // const moodsId = parseInt(moodBtn.dataset.id, 10);
+        
+          
+
+           if(moodsId){
+            axios.delete( 'http://localhost:3004/moods/${moodsId}').then(() =>{
+                dropmoods(); 
+            })
+            }
+        }) 
+    })
+})}
+ dropmoods();
+  
+
+
+
+
+const moodform = document.querySelector('.js-mood-form');
+
+moodform.addEventListener('submit', (event) =>{
+    event.preventDefault();
+    
+    const moodnameInput =  moodform.querySelector<HTMLInputElement>('input[name="mood"]');
+    const moodnameInputValue = moodnameInput.value;
+    
+
+
+    axios.post<mood>( 'http://localhost:3004/moods', {
+        name: moodnameInputValue,
+    }).then (() =>{
+        moodnameInput.value = '';
+        dropmoods();
+    });
+
+});
+
+
+/*addButton.addEventListener("click", () =>{
+   const postResult = axios.post( 'http://localhost:3004/moods', {
         name : "ph3",
     });
-})
+    postResult.then(()=>{
+    dropmoods();
+});
+})*/
