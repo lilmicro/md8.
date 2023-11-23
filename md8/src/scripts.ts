562091614aab6bcb5412b28d0c1099643bce7be5
lilmicro <epaskevica3@gmail.com>
2023-11-23 12:01:06 +0000

    don1

--- md8/src/scripts.ts
+++ md8/src/scripts.ts
@@ -5,7 +5,7 @@ import sum from './utils/sum/sum';
 
 
 
-//const addButton = document.querySelector(".js-add-button");
+
 
 
 //const url = 'http://localhost:3004/moods';
@@ -16,9 +16,11 @@ import sum from './utils/sum/sum';
 //result.then((res) =>{
 //  console.log(res.data)
 //})
+//import axios, { AxiosError, AxiosResponse } from 'axios';
 
 
-import axios, { AxiosError, AxiosResponse } from 'axios';
+import axios from 'axios';
+ import { formatDistanceToNow } from 'date-fns';
 
 type Moods = {
     id: number;
@@ -28,80 +30,67 @@ type Moods = {
     createdDate: string;
     delete: string;
 }
-let moods : Moods[] = [];
-
- import { formatDistance } from 'date-fns'
-import { enUS } from 'date-fns/locale';
+//import { enUS } from 'date-fns/locale';
 //const datefns = require("date-fns");
 //import { format, compareAsc } from 'date-fns'
 
 const dropmoods = ()=>{
 const result = axios.get<Moods[]>(' http://localhost:3004/moods ');
-const imageInput = document.querySelector<HTMLInputElement>('.js-upload-input');
-
- 
-const wrappers = document.querySelector<HTMLDivElement>(".js-ph-wrapper");
+//const imageInput = document.querySelector<HTMLInputElement>('.js-upload-input');
+//const addButton = document.querySelector<HTMLButtonElement>(".js-add-button");
+const wrappers = document.querySelector<HTMLDivElement>('.js-ph-wrapper');
  wrappers.innerHTML = '';
 
-
-    result.then(({data}) :void =>{
-    data.forEach((Moods : Moods) => {
+result.then(({data}) :void =>{
+    data.forEach((mood) => {
         wrappers.innerHTML += `
         <div class="mood">
         <div class="image">
-        <img src="${Moods.url}" width="150" height="150" >
+        <img src="https://picsum.photos/300/300" >
         </div>
-         <h1 class= "text1">${Moods.name}</h1>
+         <h1 class= "text1">${mood.name}</h1>
          <br>
-         <h2 class= "text2">${Moods.descript}</h2>
-         <button class="js-moods-delete" data-moods-delete="${Moods.id}">delete</button>
-        <div class="date">${formatDistance(new Date (), new Date(), { locale: enUS})}</div>
+         <h2 class= "text2">${mood.descript}</h2>
+         <button class="js-moods-delete" data-moods-delete="${mood.id}">delete</button>
+        <div class="date">${formatDistanceToNow(new Date(), { addSuffix: true })}</div>
          <hr>
          </div>
-         ` 
+         ` ;
     }); 
    
-
-
 
 const moodDelete = document.querySelectorAll<HTMLButtonElement>('.js-moods-delete');
     moodDelete.forEach((moodBtn: HTMLButtonElement) =>{
         moodBtn.addEventListener('click', async () =>{
-           const moodsId   = moodBtn.dataset.moodsDelete;
+           const moodId   = moodBtn.dataset.moodsDelete;
            // const moodsId = parseInt(moodBtn.dataset.id, 10);
         
-           if(moodsId){
-            axios.delete(`http://localhost:3004/moods/${moodsId}`).then(() =>{
+           if(moodId){
+            axios.delete(`http://localhost:3004/moods/${moodId}`).then(() =>{
                 dropmoods(); 
             })
-            }
-        }) 
-    })
-}
-)};
+            };
+        }) ;
+    });
+})
+};
  dropmoods();
   
 
-
-
-
 const moodform = document.querySelector('.js-mood-form');
+const moodnameInput =  moodform.querySelector<HTMLInputElement>('input[name="mood"]');
+const descriptInput =  moodform.querySelector<HTMLInputElement>('input[name="descript"]');
+
 
 moodform.addEventListener('submit', (event) =>{
-    event.preventDefault();
-    
-    const moodnameInput =  moodform.querySelector<HTMLInputElement>('input[name="mood"]');
-    const moodnameInputValue = moodnameInput.value;
-   // const descriptInput =  moodform.querySelector<HTMLInputElement>('input[descript="mood"]');
-   // const descriptInputValue = descriptInput.value;
-
+event.preventDefault();
 
     axios.post<Moods>(  'http://localhost:3004/moods', {
-        name: moodnameInputValue,
-    //   descript : descriptInputValue,
+        name: moodnameInput.value,
+       descript: descriptInput.value,
     }).then (() =>{
         moodnameInput.value = '';
-     //   descriptInput.value = '';
+        descriptInput.value = '';
         dropmoods();
     });
 
